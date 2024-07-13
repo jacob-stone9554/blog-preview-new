e3439e229be0653531e4515fc381d80d880437d2
Jacob Stone <jumboshrimp9554@gmail.com>
2024-07-13 13:40:02 -0700

    finally got the share button working. will start on mobile view soon

--- main.js
+++ main.js
@@ -4,16 +4,20 @@
 document.addEventListener('DOMContentLoaded', () => {
     const shareButton = document.querySelector('.share-btn');
     const shareMenu = document.querySelector('.share-menu');
+    const triangle = document.querySelector('.share-menu-triangle');
+
 
     //show/hide the menu on click
     shareButton.addEventListener('click', () => {
-        shareMenu.style.display = shareMenu.style.display === 'none' || shareMenu.style.display === '' ? 'block' : 'none';
+        shareMenu.style.display = shareMenu.style.display === 'none' || shareMenu.style.display === '' ? 'flex' : 'none';
+        triangle.style.display = triangle.style.display === 'none' || triangle.style.display === '' ? 'block' : 'none';
     });
 
     //hide the menu when the target is not the share button
     document.addEventListener('click', (event) => {
         if(!shareButton.contains(event.target) && !shareMenu.contains(event.target)) {
             shareMenu.style.display = 'none';
+            triangle.style.display = 'none';
         }
     })
     
