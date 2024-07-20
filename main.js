2e98403de7e2f39c15828ba1acff98970dc47bac
Jacob Stone <jumboshrimp9554@gmail.com>
2024-07-20 12:33:39 -0700

    working on the mobile styles of the share menu

--- main.js
+++ main.js
@@ -5,13 +5,22 @@ document.addEventListener('DOMContentLoaded', () => {
     const shareButton = document.querySelector('.share-btn');
     const shareMenu = document.querySelector('.share-menu');
     const triangle = document.querySelector('.share-menu-triangle');
+    const bottomRow = document.querySelector('.bottom-row');
 
 
     //show/hide the menu on click
     shareButton.addEventListener('click', () => {
         shareMenu.style.display = shareMenu.style.display === 'none' || shareMenu.style.display === '' ? 'flex' : 'none';
         triangle.style.display = triangle.style.display === 'none' || triangle.style.display === '' ? 'block' : 'none';
+        checkTriangle();
     });
+
+    function checkTriangle() {
+        if(window.innerWidth <= 375) {
+            triangle.style.display = 'none';
+        }
+    }
+    
 
     //hide the menu when the target is not the share button
     document.addEventListener('click', (event) => {
